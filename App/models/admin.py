78475dec2605437f2db924018211fb9c1303310e
Aadil Ali <60389946+Aadil-speck@users.noreply.github.com>
2023-11-27 13:23:29 +0000

    redisgn of admin module

--- App/models/admin.py
+++ App/models/admin.py
@@ -1,27 +1,26 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import UserMixin
-from App.database import db
+from .user import User
+from App.database import db 
 
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
+class Student(User):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    profile = db.relationship('Profile', backref=db.backref('admin', lazy='joined'))
+    competition = db.relationship('Competition', backref=db.backref('competitor', lazy='joined'))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, email):
         self.username = username
         self.set_password(password)
+        self.user_type = "admin"
+        self.email = email
 
-    def get_json(self):
+
+    def __repr__(self):
+        return f'<Admin {self.id} {self.username} {self.email}>'
+    
+    def toJSON(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'type': 'admin',
+            'email': self.email
         }
-
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
-    
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
-
