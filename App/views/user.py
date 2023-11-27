02dd95530b2929e965520036d7d91f5b2588a756
Mariaheera <77857355+Mariaheera@users.noreply.github.com>
2023-11-27 21:00:21 +0000

    uncommented the first function, added some code to the last three functions

--- App/views/user.py
+++ App/views/user.py
@@ -1,3 +1,4 @@
+from App.models.user import User
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import current_user, login_required
@@ -21,10 +22,12 @@ from App.controllers import (
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
-# @user_views.route('/users', methods=['GET'])
-# def get_user_page():
-#     users = get_all_users()
-#     return render_template('users.html', users=users)
+@user_views.route('/users', methods=['GET'])
+def get_user_page():
+    users = get_all_users()
+    return render_template('users.html', users=users)
+
+#this was commented off
 
 @user_views.route('/api/users', methods=['GET'])
 def get_users_action():
@@ -34,11 +37,8 @@ def get_users_action():
 @user_views.route('/api/users', methods=['POST'])
 def create_user_endpoint():
     data = request.json
-    response = create_user(data['username'], data['password'])
-    if response:
-        return (jsonify({'message': f"user created"}),201)
-    return (jsonify({'error': f"error creating user"}),500)
-
+    create_user(data['username'], data['password'])
+    return jsonify({'message': f"user {data['username']} created"})
 
 @user_views.route('/users', methods=['POST'])
 def create_user_action():
@@ -52,10 +52,15 @@ def static_user_page():
   return send_from_directory('static', 'static-user.html')
 
 
-@user_views.route('/tester', methods=['GET'])
-def random_function():
-    flash(f"hello user this test has been successful") 
-    return "yes"
+@user_views.route('/normal', methods=['POST'])
+def create_normal_user_action():
+    data = request.json
+    check = User.query.filter_by(username=data['username']).first()
+    if not check:
+        result = create_user(username=data['username'], password=data['password'], email=data['email'])
+        if result:
+            return jsonify({"message": f"User created with id {result.id}"}), 201
+    return jsonify({"error": f"Username {data['username']} already exists "}), 500
 
 
 @user_views.route('/users/rankings', methods=['GET'])
