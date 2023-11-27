da5a4cd14d326f5cc1a00212111fc81ce2e850b5
Aadil Ali <60389946+Aadil-speck@users.noreply.github.com>
2023-11-27 13:25:11 +0000

    creation of notification specal feat modle

---
+++ App/models/notification.py
@@ -0,0 +1,24 @@
+from App.database import db
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    rank = db.Column(db.Integer, db.ForeignKey('ranking.id'))
+    message = db.Column(db.String, nullable=False)
+    read = db.Column(db.Boolean, default=False)
+    
+
+    def __init__(self, userId, message, rank):
+        self.userId = userId
+        self.rank = rank
+        self.message = message
+        self.read = False
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'message': self.message,
+            'rank': self.rank,
+            'read': self.read
+        }
